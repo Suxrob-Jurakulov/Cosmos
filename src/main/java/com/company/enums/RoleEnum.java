27ca9c9d8aa3fa6c53fa19a1872c8fd2d2ac119e
SUKHROB <suxrobbek997@gmail.com>
2022-11-24 17:25:06 +0500

    Implements UserDetails

---
+++ src/main/java/com/company/enums/RoleEnum.java
@@ -0,0 +1,5 @@
+package com.company.enums;
+
+public enum RoleEnum {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/company/model/UserEntity.java
@@ -0,0 +1,67 @@
+package com.company.model;
+
+import com.company.enums.RoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private RoleEnum role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
